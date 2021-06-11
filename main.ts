5a0d282c9da2c4c02ecf0aa4191e7bce9c172e7f
Hi6888 <85736613+Hi6888@users.noreply.github.com>
2021-06-11 17:50:02 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showNumber(603)
+    basic.showIcon(IconNames.Heart)
+    basic.showString("graduation")
+    basic.clearScreen()
+    basic.showString("bey")
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
